ca904305a9942b28d38d04013b0f19be86a8b280
bucard <rafaelbucard@gmail.com>
2021-03-27 06:14:29 -0300

    criando versionamento git

---
+++ app/Http/Livewire/ItemList.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ItemList extends Component
+{
+    public function render()
+    {
+        return view('livewire.item-list');
+    }
+}
